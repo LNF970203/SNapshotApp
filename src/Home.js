7a8d655bbee0c6df8db6af435f4be15c2c2f5b1d
LNF970203 <lachinnanu123@gmail.com>
2023-06-03 13:05:06 +0530

    add: styles

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import "bootstrap/dist/css/bootstrap.css";
+import "./Home.css";
 
-library.add(faMagnifyingGlass)
+library.add(faMagnifyingGlass);
 
 const HomePage = (props) => {
   const [fieldValue, SetFieldValue] = useState(null);
@@ -14,33 +16,42 @@ const HomePage = (props) => {
   };
 
   return (
-    <div>
-      <h1>SnapShot</h1>
-      <form>
-        <input
-          type="text"
-          name="keyword"
-          placeholder="Enter the Keyword"
-          value={fieldValue || ""}
-          onChange={handleChange}
-        />
-        <button disabled={fieldValue ? false : true}>
-            <FontAwesomeIcon icon="fa-magnifying-glass" />
-        </button>
-      </form>
-      <div>
-        <button>
-            Mountain
-        </button>
-        <button>
-            Beaches
-        </button>
-        <button>
-            Birds
-        </button>
-        <button>
-            Foods
-        </button>
+    <div className="main-div">
+      <div className="container-fluid">
+        <h1 className="main-heading">SnapShot</h1>
+        <form className="mb-4">
+          <div className="container-fluid w-75 mx-auto p-0">
+            <input
+              className="input-text"
+              type="text"
+              name="keyword"
+              placeholder="Enter the Keyword"
+              value={fieldValue || ""}
+              onChange={handleChange}
+            />
+            <button 
+            disabled={fieldValue ? false : true} 
+            className="input-search">
+              <FontAwesomeIcon icon="fa-magnifying-glass" />
+            </button>
+          </div>
+        </form>
+        <div id="button-holder">
+          <div className="container-fluid w-75 mx-auto p-0 d-flex justify-content-between">
+            <div className="button-div">
+                <button className="nav-button">Mountain</button>
+            </div>
+            <div className="button-div">
+                <button className="nav-button">Beaches</button>
+            </div>
+            <div className="button-div">
+                <button className="nav-button">Birds</button>
+            </div>
+            <div className="button-div">
+                <button className="nav-button">Foods</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
